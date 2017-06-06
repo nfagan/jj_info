4d6335aa7544e84a631d702160a246051c062387
nfagan <nick@fagan.org>
2017-06-06 17:05:35 -0400

    added juice switch

--- +jj_info/setup.m
+++ +jj_info/setup.m
@@ -1,68 +1,90 @@
 function opts = setup()
 
-addpath( genpath('C:\Repositories\ptb_helpers') );
-addpath( 'C:\Repositories\serial_comm' );
-
 % - STATES - %
 STATES.sequence = { 'new_trial', 'fixation', 'display_random_vs_info_cues' ...
-  , 'look_to_random_vs_info', 'display_info_cues', 'reward', 'iti' };
-
-% - SCREEN + WINDOW - %
-SCREEN = ScreenManager();
-WINDOW = SCREEN.open_window( 2, [0 0 0] );
+  , 'look_to_random_vs_info', 'display_info_cues', 'reward', 'iti' ...
+  , 'display_social_image' };
 
 % - IO - %
-IO.edf_file = '05_19TaB.edf';
-IO.edf_folder = 'C:\Repositories\jj_info\data';
-IO.data_file = '05_19TaB.mat';
-IO.data_folder = 'C:\Repositories\jj_info\data';
+% IO.repo_folder = 'C:\Repositories';
+IO.repo_folder = 'C:\Users\changLab\Repositories';
+IO.edf_file = '06_06TaB';
+IO.data_file = '06_06TaB.mat';
+IO.edf_folder = fullfile( IO.repo_folder, 'jj_info', 'data' );
+IO.data_folder = fullfile( IO.repo_folder, 'jj_info', 'data' );
+IO.stimuli_path = fullfile( IO.repo_folder, 'jj_info', 'stimuli' );
 
-IO.stimuli_path = 'C:\Repositories\jj_info\stimuli';
+addpath( genpath(fullfile(IO.repo_folder, 'ptb_helpers')) );
+addpath( fullfile(IO.repo_folder, 'serial_comm') );
 
 jj_info.util.assert__is_valid_path( IO.data_folder );
-assert__file_does_not_exist( fullfile(IO.data_folder, IO.data_file) );
-assert__file_does_not_exist( fullfile(IO.edf_folder, IO.edf_file) );
+% assert__file_does_not_exist( fullfile(IO.data_folder, IO.data_file) );
+% assert__file_does_not_exist( fullfile(IO.edf_folder, IO.edf_file) );
 
 % - META - %
 META.monkey = 'Tarantino';
-META.date = '05/19/17';
+META.date = '06/06/17';
 META.session = '2';
+META.notes = ' 500 choice, 100 initial fix';
+
+% - INTERFACE - %
+INTERFACE.use_eyelink = false;
+INTERFACE.use_arduino = false;
+
+IS_JUICE = false;
+
+% - SCREEN + WINDOW - %
+SCREEN = ScreenManager();
+WINDOW = SCREEN.open_window( 0, [0 0 0] );
 
 % - SERIAL - %
 port = 'COM4';
 messages = struct( 'message', 'clock_synch', 'char', 'C' );
 reward_chars = { 'A' };
 serial_manager = serial_comm.SerialManager( port, messages, reward_chars );
-serial_manager.start();
+if ( INTERFACE.use_arduino )
+  serial_manager.start();
+end
 
 SERIAL.serial_manager = serial_manager;
 
 % - EYE TRACKER - %
 TRACKER = EyeTracker( IO.edf_file, IO.edf_folder, WINDOW.index );
-TRACKER.bypass = false;
+TRACKER.bypass = ~INTERFACE.use_eyelink;
 TRACKER.init();
+
+if ( IS_JUICE )
+  reward_types = { 'big', 'small' };
+else
+  reward_types = { 'threat', 'neutral' };
+end
 
 % - STRUCTURE - %
 choice_combs = jj_info.util.allcomb( { ...
     { 'choice' } ...
-  , { 'big', 'small' } ...
+  , reward_types ...
   , { 'rand1', 'rand2' } ...
   , { {'center-left', 'center-right'}, {'center-right', 'center-left'} }
 } );
 info_combs = jj_info.util.allcomb( { ...
     { 'info' } ...
-  , { 'big', 'small' } ...
+  , reward_types ...
   , { 'rand1' } ...
   , { 'center-left', 'center-right' } ...
 } );
 rand_combs = jj_info.util.allcomb( { ...
     { 'random' } ...
-  , { 'big', 'small' } ...
+  , reward_types ...
   , { 'rand1', 'rand2' } ...
   , { 'center-left', 'center-right' } ...
 } );
 
-STRUCTURE.block_sequence = [ choice_combs; info_combs; info_combs; rand_combs ];
+bs = [ choice_combs; info_combs; info_combs; rand_combs ];
+bs_ids = arrayfun( @(x) {x}, 1:size(bs, 1) );
+bs(:, end+1) = bs_ids(:);
+
+STRUCTURE.block_sequence = bs;
+STRUCTURE.IS_JUICE = IS_JUICE;
 
 % - TIMINGS - %
 time_in.task = Inf;
@@ -70,13 +92,14 @@ time_in.trial = Inf;
 time_in.new_trial = 0;
 time_in.fixation = Inf;
 time_in.display_random_vs_info_cues = 0;
-time_in.look_to_random_vs_info = 0.6; %2
+time_in.look_to_random_vs_info = 0.5; %2
 time_in.display_info_cues = 2;
+time_in.display_social_image = 2;
 time_in.reward = 0;
 time_in.iti = 1;
 time_in.error = 3;
 
-fixations.fix_square = .2; %go to .8 for real task
+fixations.fix_square = .1; %go to .8 for real task
 fixations.receive_info_cue = .05; %go to 1 for real task
 fixations.receive_random_cue = .05; %go to 1 for real task
 fixations.make_choice = .05; %go to 1 for real task
@@ -86,10 +109,17 @@ TIMER = Timer();
 TIMER.register( time_in );
 
 % - STIMULI - %
-images.info.big = get_images( fullfile(IO.stimuli_path, 'information', 'big') );
-images.info.small = get_images( fullfile(IO.stimuli_path, 'information', 'small') );
+if ( IS_JUICE )
+  images.info.big = get_images( fullfile(IO.stimuli_path, 'information', 'big') );
+  images.info.small = get_images( fullfile(IO.stimuli_path, 'information', 'small') );
+else
+  images.info.neutral = get_images( fullfile(IO.stimuli_path, 'information', 'big') );
+  images.info.threat = get_images( fullfile(IO.stimuli_path, 'information', 'small') );
+end
 images.random.rand1 = get_images( fullfile(IO.stimuli_path, 'random', 'rand1') );
 images.random.rand2 = get_images( fullfile(IO.stimuli_path, 'random', 'rand2') );
+images.social.neutral = get_images( fullfile(IO.stimuli_path, 'social_images', 'neutral') );
+images.social.threat = get_images( fullfile(IO.stimuli_path, 'social_images', 'threat') );
 
 sounds.error = get_sounds( fullfile(IO.stimuli_path, 'sounds', 'error') );
 sounds.reward = get_sounds( fullfile(IO.stimuli_path, 'sounds', 'reward') );
@@ -101,16 +131,18 @@ fix_square.make_target( TRACKER, fixations.fix_square );
 fix_square.blink( .5 );
 
 receive_info_cue = WINDOW.Rectangle( [350, 350] );
-receive_info_cue.color = [ 42, 172, 227 ];
+% receive_info_cue.color = [ 42, 172, 227 ];
+receive_info_cue.color = [ 247, 148, 30 ];
 receive_info_cue.put( 'center' );
 receive_info_cue.make_target( TRACKER, fixations.receive_info_cue );
 
 receive_random_cue = WINDOW.Rectangle( [350, 350] );
-receive_random_cue.color = [ 247, 148, 30 ];
+% receive_random_cue.color = [ 247, 148, 30 ];
+receive_random_cue.color = [ 42, 172, 227 ];
 receive_random_cue.put( 'center' );
 receive_random_cue.make_target( TRACKER, fixations.receive_random_cue );
 
-info_cue = WINDOW.Image( [400, 400], images.info.small.matrices{1} );
+info_cue = WINDOW.Image( [400, 400], images.info.(reward_types{1}).matrices{1} );
 info_cue.color = [ 200, 200, 10 ];
 info_cue.put( 'center-left' );
 
@@ -118,15 +150,21 @@ random_cue = WINDOW.Image( [400, 400], images.random.rand1.matrices{1} );
 random_cue.color = [ 20, 50, 100 ];
 random_cue.put( 'center-right' );
 
+social_image = WINDOW.Image( [400, 400], images.social.neutral.matrices{1} );
+social_image.color = [ 20, 50, 100 ];
+social_image.put( 'center' );
+
 STIMULI.fix_square = fix_square;
 STIMULI.receive_info_cue = receive_info_cue;
 STIMULI.receive_random_cue = receive_random_cue;
 STIMULI.info_cue = info_cue;
 STIMULI.random_cue = random_cue;
+STIMULI.social_image = social_image;
 STIMULI.images = images;
 STIMULI.sounds = sounds;
 
 % - REWARDS - %
+REWARDS.social = 300;
 REWARDS.big = 600;
 REWARDS.small = 50;
 REWARDS.random = [ 100, 300 ];
@@ -137,6 +175,7 @@ opts.STRUCTURE = STRUCTURE;
 opts.SCREEN = SCREEN;
 opts.WINDOW = WINDOW;
 opts.IO = IO;
+opts.INTERFACE = INTERFACE;
 opts.META = META;
 opts.TRACKER = TRACKER;
 opts.TIMER = TIMER;
--- +jj_info/task.m
+++ +jj_info/task.m
@@ -6,12 +6,20 @@ TIMER =           opts.TIMER;
 TRACKER =         opts.TRACKER;
 STIMULI =         opts.STIMULI;
 STRUCTURE =       opts.STRUCTURE;
+INTERFACE =       opts.INTERFACE;
 REWARDS =         opts.REWARDS;
 serial_manager =  opts.SERIAL.serial_manager;
 
 cstate = 'new_trial';
 
 first_entry = true;
+
+IS_JUICE = STRUCTURE.IS_JUICE;
+if ( IS_JUICE )
+  task_type = 'juice';
+else
+  task_type = 'social';
+end
 
 PROGRESS = struct();
 DATA = struct();
@@ -35,21 +43,25 @@ while ( true )
   %   new trial
   if ( isequal(cstate, 'new_trial') )
     if ( first_entry )
+      any_errors = any( structfun(@(x) x, errors) );
       %   RECORD DATA
       if ( TRIAL_NUMBER > 0 )
         tn = TRIAL_NUMBER;
+        DATA(tn).task_type = task_type;
         DATA(tn).trial_number = tn;
         DATA(tn).block_number = block_number;
         DATA(tn).trial_type = trial_type;
         DATA(tn).selected_cue = chosen_option;
-        DATA(tn).shown_reward_cue = shown_image;
+        DATA(tn).shown_reward_cue = shown_reward_cue;
         DATA(tn).reward_type = rwd_type;
+        DATA(tn).shown_social_image = social_image_file;
         DATA(tn).reward_size = current_reward;
         DATA(tn).info_location = info_location;
         DATA(tn).random_location = random_location;
+        DATA(tn).block_sequence_id = sequence_id;
         DATA(tn).errors = errors;
         DATA(tn).events = PROGRESS;
-        if ( ~isempty(chosen_option) )
+        if ( ~any_errors )
           if ( isequal(trial_type, 'choice') )
             was_choice = true;
           else was_choice = false;
@@ -83,11 +95,13 @@ while ( true )
       end
       TRIAL_NUMBER = TRIAL_NUMBER + 1;
       PROGRESS = structfun( @(x) NaN, PROGRESS, 'un', false );
+      PROGRESS.trial_start = TIMER.get_time( 'task' );
+      TRACKER.send( sprintf('TRIAL__%d', TRIAL_NUMBER) );
       TIMER.reset_timers( cstate );
-      if ( ~errors.no_choice && ~errors.broke_choice )
+      if ( ~any_errors )
         %   reset variables
         chosen_option = [];
-        shown_image = [];
+        shown_reward_cue = [];
         %   choose a new sequence
         current_sequence_ind = randperm( size(block_sequence, 1) );
         current_sequence = block_sequence( current_sequence_ind(1), : );
@@ -97,6 +111,7 @@ while ( true )
         rwd_type = current_sequence{2};
         random_cue_type = current_sequence{3};
         location = current_sequence{4};
+        sequence_id = current_sequence{5};
         %   determine which stimuli to show, info v. random v. both
         images = STIMULI.images;
         receive_info_cue = STIMULI.receive_info_cue;
@@ -142,7 +157,24 @@ while ( true )
             random_cue.put( random_location );
         end
         %   establish current_reward size
-        current_reward = REWARDS.(rwd_type);
+        if ( IS_JUICE )
+          current_reward = REWARDS.(rwd_type);
+        else
+          current_reward = REWARDS.social;
+        end
+        %   establish which image to show, if the task type is social
+        if ( ~IS_JUICE )
+          social_image_matrices = images.social.(rwd_type).matrices;
+          social_image_files = images.social.(rwd_type).filenames;
+          n_images = numel( social_image_matrices );
+          perm_ind = randperm( n_images );
+          social_image_matrices = social_image_matrices( perm_ind );
+          social_image_files = social_image_files( perm_ind );
+          STIMULI.social_image.image = social_image_matrices{1};
+          social_image_file = social_image_files{1};
+        else
+          social_image_file = '';
+        end
       end
       Screen( 'Flip', opts.WINDOW.index );
       first_entry = false;
@@ -164,6 +196,7 @@ while ( true )
       fix_square.reset_targets();
       made_look = false;
       errors.broke_fixation = false;
+      errors.no_fixation = false;
       first_entry = false;
     end
     TRACKER.update_coordinates();
@@ -236,6 +269,8 @@ while ( true )
       TIMER.reset_timers( cstate );
       cellfun( @(x) x.reset_targets(), choice_options.stimuli );
       chosen_option = [];
+      errors.broke_choice = false;
+      errors.no_choice = false;
       made_choice = false;
       first_entry = false;
     end
@@ -308,14 +343,34 @@ while ( true )
     end
     if ( isequal(chosen_option, 'info') )
       cue = STIMULI.info_cue;
-      shown_image = info_image_file;
+      shown_reward_cue = info_image_file;
     else
       cue = STIMULI.random_cue;
-      shown_image = rand_image_file;
+      shown_reward_cue = rand_image_file;
     end
     cue.draw();
     Screen( 'Flip', opts.WINDOW.index );
     if ( TIMER.duration_met('display_info_cues') )
+      if ( IS_JUICE )
+        cstate = 'reward';
+      else
+        cstate = 'display_social_image';
+      end
+      first_entry = true;
+    end
+  end
+  
+  %   display_social_image
+  if ( isequal(cstate, 'display_social_image') )
+    if ( first_entry )
+      PROGRESS.(cstate) = TIMER.get_time( 'task' );
+      TIMER.reset_timers( cstate );
+      social_image = STIMULI.social_image;
+      first_entry = false;
+    end
+    social_image.draw();
+    Screen( 'Flip', opts.WINDOW.index );
+    if ( TIMER.duration_met('display_social_image') )
       cstate = 'reward';
       first_entry = true;
     end
@@ -326,7 +381,9 @@ while ( true )
     if ( first_entry )
       PROGRESS.(cstate) = TIMER.get_time( 'task' );
       TIMER.reset_timers( cstate );
-      serial_manager.reward( 'A', current_reward );
+      if ( INTERFACE.use_arduino )
+        serial_manager.reward( 'A', current_reward );
+      end
       sounds = STIMULI.sounds.reward;
       sound( sounds.matrices{1}, sounds.fs{1} );
       %   deliver_reward( current_reward )
@@ -353,7 +410,9 @@ while ( true )
     end
   end
   
-  serial_manager.update();
+  if ( INTERFACE.use_arduino )
+    serial_manager.update();
+  end
   
   %   Quit if error in EyeLink
   err = TRACKER.check_recording();
@@ -372,6 +431,7 @@ end
 data = struct();
 data.DATA = DATA;
 data.META = META;
+data.opts = opts;
 
 save( fullfile(IO.data_folder, IO.data_file), 'data' );
 
